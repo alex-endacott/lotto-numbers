1d2a58529dbf09dfb4eab67c00761fce904b0f56
Alex Endacott <alex@whysie.com>
2021-04-11 12:14:27 -0500

    remove pycache

--- inc/powerball.py
+++ inc/powerball.py
@@ -5,7 +5,7 @@ All the logic for analysing winning Powerball numbers
 
 import pandas as pd
 
-PB_DATA_PATH = 'prior_winners/powerball.csv'
+PB_PRIOR_WINNERS_DATA_PATH = 'prior_winners/powerball.csv'
 
 
 def get_prior_pb_winners_data():
@@ -13,4 +13,6 @@ def get_prior_pb_winners_data():
     Reads in Powerball csv data
     :return:
     """
-    pd.read_csv(PB_DATA_PATH)
+    pb_df = pd.read_csv(PB_PRIOR_WINNERS_DATA_PATH)
+    pb_df.columns = ['num1', 'num2', 'num3', 'num4', 'num5', 'pb']
+    print(pb_df.iloc[0])
