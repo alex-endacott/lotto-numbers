02284bf2f2fbe748244781a7bab659e238b0428b
Alex Endacott <alex@whysie.com>
2021-05-22 11:09:15 -0500

    add data sources to doc string

--- main.py
+++ main.py
@@ -2,6 +2,12 @@
 A program to generate lotto numbers for the Mega Millions
 and Powerball lotteries using patterns recognized from
 previous winning numbers.
+
+PowerBall data collected from:
+https://www.txlottery.org/export/sites/lottery/Games/Powerball/Winning_Numbers/
+
+Mega Millions data collected from:
+https://www.txlottery.org/export/sites/lottery/Games/Mega_Millions/Winning_Numbers/
 """
 
 import inc.powerball as pb
